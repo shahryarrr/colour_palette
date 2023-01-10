31ea164a3dafc1d5c42658d47ad8e646cfa9bf00
shahryarrr <107395895+shahryarrr@users.noreply.github.com>
2023-01-10 22:27:47 +0000

    HTML & CSS

---
+++ main.js
@@ -0,0 +1,142 @@
+
+
+const namedColors = [
+Maroon
+DarkRed
+Fire Brick
+Red
+Salmon
+Tomato
+Coral
+OrangeRed
+Chocolate
+Sandy Brown
+Dark Orange
+Orange
+DarkGoldenrod
+Goldenrod
+Gold
+Olive
+Yellow
+Yellow Green
+Green Yellow
+Chartreuse
+Lawn Green
+Green
+Lime
+Lime Green
+Spring Green
+Medium Spring Green
+Turquoise
+LightSeaGreen
+MediumTurquoise
+Dark Cyan
+Aqua
+Dark Turquoise
+Deep Sky Blue
+Dodger Blue
+Royal Blue
+Navy
+Dark Blue
+Medium Blue
+Blue
+Blue Violet
+Dark Orchid
+Dark Violet
+Purple
+Dark Magenta
+Magenta
+Medium Violet Red
+Deep Pink
+Hot Pink
+Crimson
+Brown
+Indian Red
+Rosy Brown
+Light Coral
+Snow
+Misty Rose
+Dark Salmon
+Light Salmon
+Sienna
+Sea Shell
+Saddle Brown
+Peachpuff
+Peru
+Linen
+Bisque
+Burlywood
+Tan
+Antique White
+Navajo White
+Blanched Almond
+Papaya Whip
+Moccasin
+Wheat
+Oldlace
+FloralWhite
+Cornsilk
+Khaki
+Lemon Chiffon
+Pale Goldenrod
+Dark Khaki
+Beige
+Light Goldenrod Yellow
+Light Yellow
+Ivory
+Olive Drab
+Dark Olive Green
+Dark Sea Green
+Dark Green
+ForestGreen
+Light Green
+PaleGreen
+Honeydew
+Sea Green
+Medium Sea Green
+Mintcream
+Medium Aquamarine
+Aquamarine
+Dark Slate Gray
+Pale Turquoise
+Light Cyan
+Azure
+Cadet Blue
+Powder Blue
+Light Blue
+Sky Blue
+Lightsky Blue
+Steel Blue
+Alice Blue
+Slate Gray
+Light Slate Gray
+Lightsteel Blue
+Cornflower Blue
+Lavender
+Ghost White
+Midnight Blue
+Slate Blue
+Dark Slate Blue
+Medium Slate Blue
+Medium Purple
+Indigo
+Medium Orchid
+Plum
+Violet
+Thistle
+Orchid
+LavenderB lush
+Pale Violet Red
+Pink
+LightPink
+Black
+DimGray
+Gray
+Dark Gray
+Silver
+Light Grey
+Gainsboro
+White Smoke
+White
+
+]
